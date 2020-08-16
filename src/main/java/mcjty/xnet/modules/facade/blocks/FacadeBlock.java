cbc8d45eaf9313ff08cd8571a2853aca7e551eae
McJty <mcjty1@gmail.com>
2020-08-16 07:25:19 +0200

    Removed OnlyIn. It's never almost never needed and it's a hack. Forge devs have been trying to get people to not use it for a while now

--- src/main/java/mcjty/xnet/modules/facade/blocks/FacadeBlock.java
+++ src/main/java/mcjty/xnet/modules/facade/blocks/FacadeBlock.java
@@ -15,9 +15,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.common.util.Constants.BlockFlags;
 
 import javax.annotation.Nullable;
