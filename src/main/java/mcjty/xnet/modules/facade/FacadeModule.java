831c7c4a8e2753610076d4fcb5d01d80f3e7814d
McJty <mcjty1@gmail.com>
2023-01-30 15:10:52 +0100

    Added facade to the creative tab

--- src/main/java/mcjty/xnet/modules/facade/FacadeModule.java
+++ src/main/java/mcjty/xnet/modules/facade/FacadeModule.java
@@ -17,12 +17,13 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.registries.RegistryObject;
 
 import static mcjty.lib.datagen.DataGen.has;
+import static mcjty.xnet.XNet.tab;
 import static mcjty.xnet.setup.Registration.*;
 
 public class FacadeModule implements IModule {
 
     public static final RegistryObject<FacadeBlock> FACADE = BLOCKS.register("facade", () -> new FacadeBlock(GenericCableBlock.CableBlockType.FACADE)); // @todo 1.14
-    public static final RegistryObject<Item> FACADE_ITEM = ITEMS.register("facade", () -> new FacadeBlockItem(FACADE.get()));
+    public static final RegistryObject<Item> FACADE_ITEM = ITEMS.register("facade", tab(() -> new FacadeBlockItem(FACADE.get())));
     public static final RegistryObject<BlockEntityType<?>> TYPE_FACADE = TILES.register("facade", () -> BlockEntityType.Builder.of(FacadeTileEntity::new, FACADE.get()).build(null));
 
     @Override
